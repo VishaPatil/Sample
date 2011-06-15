14652e4f00d884fecc4f7e6ec0338f5541e62650
unknown <patilv@.wharton.upenn.edu>
2011-06-15 11:34:38 -0400

    SAMPLE CRUD WEBPAGE

---
+++ WebSite1/Sample.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Sample.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+    <style type="text/css">
+        .style1
+        {
+            margin-left: 30px;
+        }
+        .style2
+        {
+            margin-left: 209px;
+        }
+        .style3
+        {
+            margin-left: 34px;
+        }
+        .style4
+        {
+            margin-left: 86px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Welcome to SAMPLE ASP.NET APPLICATION!
+    </h2>
+    FIRSTNAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="firstname" runat="server" CssClass="style1" 
+        Width="153px"></asp:TextBox>
+&nbsp;<br />
+    LASTNAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="lastname" runat="server" CssClass="style3" 
+        Width="151px"></asp:TextBox>
+    <br />
+    ID(PRIMARY KEY)<asp:TextBox ID="id" runat="server" CssClass="style4" 
+         Width="152px"></asp:TextBox>
+    <br />
+    <asp:Button ID="Add" runat="server" CssClass="style2" onclick="Add_Click" 
+        Text="Add" Width="66px" />
+&nbsp;<asp:Button ID="Del" runat="server" CssClass="style2" onclick="Del_Click" 
+        Text="Delete" Width="66px" />
+    <asp:Button ID="Update" runat="server" CssClass="style2" onclick="Update_Click" 
+        Text="Update" Width="66px" />
+    <br />
+</asp:Content>
---
+++ WebSite1/Sample.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection myConnection;
+    SqlCommand myCommand;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        myConnection = new SqlConnection("server=localhost;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=sample; " +
+                                       "connection timeout=30");
+
+        try
+        {
+            myConnection.Open();
+             myCommand = new SqlCommand("Command_String", myConnection);
+            Console.WriteLine("Database Connectivity Established");
+        }
+        catch(Exception ex){
+            Console.WriteLine("Error While Connecting to Database"+ex.ToString());
+        }
+
+
+    }
+
+
+    protected void Add_Click(object sender, EventArgs e)
+    {
+        Console.WriteLine("Adding Person Information to Database");
+       
+        string commandText = "INSERT INTO person (firstname, lastname, id) " +
+                        "Values ('" + firstname.Text + "','" + lastname.Text + "', " + id.Text + ")";
+        myCommand.CommandText = commandText;
+        try
+        {
+            myCommand.ExecuteNonQuery();
+            myConnection.Close();
+        }
+        catch(Exception excp)
+        {
+            Console.WriteLine(excp.ToString());
+        }
+    }
+
+    protected void Del_Click(object sender, EventArgs e)
+    {
+         Console.WriteLine("Deleting Person Information From Database");
+         string commandText = "DELETE FROM person " +
+                        "WHERE id=" + id.Text + ""; 
+         myCommand.CommandText =commandText;
+         try
+         {
+            myCommand.ExecuteNonQuery();
+            myConnection.Close();
+         }
+         catch(Exception excp)
+         {
+            Console.WriteLine(excp.ToString());
+         }
+    }
+    
+    protected void Update_Click(object sender, EventArgs e)
+    {
+        Console.WriteLine("Updating Person Information From Database");
+        string commandText = "UPDATE person " +
+                            "SET firstname='"+firstname.Text+"',lastname='"+lastname.Text+
+                       "' WHERE id=" + id.Text;
+        myCommand.CommandText = commandText;
+        try
+        {
+            myCommand.ExecuteNonQuery();
+            myConnection.Close();
+        }
+        catch (Exception excp)
+        {
+            Console.WriteLine(excp.ToString());
+        }
+
+    }
+}
